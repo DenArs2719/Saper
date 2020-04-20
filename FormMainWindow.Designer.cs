38376e5f802864589fb4cc9172f30c08dc1fe354
DenArs2719 <yevseyenkod@mail.ru>
2020-04-20 10:25:42 +0200

    Saper

---
+++ FormMainWindow.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Saper
+{
+    partial class FormMainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.easyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mediumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panelButton = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip";
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.easyToolStripMenuItem,
+            this.mediumToolStripMenuItem,
+            this.hardToolStripMenuItem});
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(74, 29);
+            this.gameToolStripMenuItem.Text = "Game";
+            // 
+            // easyToolStripMenuItem
+            // 
+            this.easyToolStripMenuItem.Name = "easyToolStripMenuItem";
+            this.easyToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.easyToolStripMenuItem.Text = "Easy";
+            this.easyToolStripMenuItem.Click += new System.EventHandler(this.easyToolStripMenuItem_Click);
+            // 
+            // mediumToolStripMenuItem
+            // 
+            this.mediumToolStripMenuItem.Name = "mediumToolStripMenuItem";
+            this.mediumToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.mediumToolStripMenuItem.Text = "Medium";
+            this.mediumToolStripMenuItem.Click += new System.EventHandler(this.mediumToolStripMenuItem_Click);
+            // 
+            // hardToolStripMenuItem
+            // 
+            this.hardToolStripMenuItem.Name = "hardToolStripMenuItem";
+            this.hardToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.hardToolStripMenuItem.Text = "Hard";
+            this.hardToolStripMenuItem.Click += new System.EventHandler(this.hardToolStripMenuItem_Click);
+            // 
+            // panelButton
+            // 
+            this.panelButton.AutoSize = true;
+            this.panelButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panelButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelButton.Location = new System.Drawing.Point(0, 33);
+            this.panelButton.Name = "panelButton";
+            this.panelButton.Size = new System.Drawing.Size(800, 417);
+            this.panelButton.TabIndex = 0;
+            this.panelButton.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // FormMainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelButton);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormMainWindow";
+            this.Text = "Saper";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem easyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mediumToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hardToolStripMenuItem;
+        private System.Windows.Forms.Panel panelButton;
+    }
+}
+
---
+++ FormMainWindow.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Saper
+{
+    public partial class FormMainWindow : Form
+    {
+        private SapperLogic myGame;
+        private const int fieldSize = 30; ///wielkość pojedynczego buttona w pikselach
+        public FormMainWindow()
+        {
+            InitializeComponent();
+
+            ///wywolanie metody tworzącej na start prostej gry
+            easyToolStripMenuItem_Click(null, null);
+        }
+
+        private void easyToolStripMenuItem_Click(object p1, object p2)
+        {
+            myGame = new SapperLogic(8, 8, 10);
+            generateView();
+        }
+        private void panel2_Paint(object p1, object p2)
+        {
+
+        }
+
+        private void mediumToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            myGame = new SapperLogic(12, 10, 25);
+            generateView();
+        }
+
+        private void hardToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            myGame = new SapperLogic(20, 15, 50);
+            generateView();
+        }
+
+        ///tworzenie nowej planszy
+        private void generateView()
+        {
+            panelButton.Controls.Clear(); ///kasowanie starych buttonów
+
+            ///generowanie nowych buttonów
+            for(int x = 0;x<myGame.BoardWidth;x++)
+            {
+                for(int y=0;y<myGame.BoardHeight;y++)
+                {
+                    Button b = new Button(); ///tworzy nowy button
+                    b.Size = new Size(fieldSize, fieldSize);
+                    b.Location = new Point(fieldSize * x, fieldSize * y);
+                    b.Click += B_Click;
+                    panelButton.Controls.Add(b);
+
+                    //Tag jest typu object i można tam wstawić cokolwiek
+                    //Trzeba jednak później pamiętać o kontroli i konwersji typów
+                    //Każdy przycisk oznaczam przy pomocy jego logicznego położenia w odniesienu do planszy gry
+                    b.Tag = new Point(x, y);
+                }
+            }
+        }
+
+        private void B_Click(object sender, EventArgs e) ///obsluga kliknięncia na battony pod czas gry
+        {
+            if(myGame.State == SapperLogic.GameState.InProgress)
+            {
+                if(sender is Button)
+                {
+                    Button b = sender as Button;
+                    if(b.Tag is Point)
+                    {
+                        Point p = (Point)b.Tag;
+
+                        myGame.Uncover(p);
+                        refreshView();
+
+                        if(myGame.State == SapperLogic.GameState.Win)
+                        {
+                            MessageBox.Show("Win");
+                        }
+                        else if (myGame.State == SapperLogic.GameState.Loss)
+                        {
+                            MessageBox.Show("Loss");
+                        }
+                    }
+                }
+            }
+            
+        }
+
+        private void refreshView()
+        {
+            foreach(Button b in panelButton.Controls) ///przechodzimy po wszystkich buttonach tworzanych w grę
+            {
+                ///pobieram z gry pole powiązane z danym buttonem
+                SapperLogic.Field f = myGame.GetFiled((Point)b.Tag);
+
+                if(f.Covered == false) ///pole jest otkryte
+                {
+                    if(f.FieldType == SapperLogic.FieldTypeEnum.Bomb) ///jeżeli bomba
+                    {
+                        b.BackColor = Color.Red;
+                        b.Text = "@";
+                    }
+                    else ///pole puste lub pole z cyframi
+                    {
+                        b.BackColor = Color.White; ///dla obydwo rodzajów pól
+                        
+                        if(f.FieldType == SapperLogic.FieldTypeEnum.BombCount) ///tylko dla pól z cyframi
+                        {
+                            b.Text = f.BombCount.ToString();
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
